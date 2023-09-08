bfbd7783bb411a2e01a833d010d63fff03e8b9ee
djamkenny <beamahnenoch5@gmail.com>
2023-09-07 19:08:57 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
-#include <stdio.h>
-/*print string*/
-int main (void)
+#include  <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
 {
-	char string[] = "\"Programming is like building a multilingual puzzle";
-	puts(string);
-
-	return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+		return (0);
 }
