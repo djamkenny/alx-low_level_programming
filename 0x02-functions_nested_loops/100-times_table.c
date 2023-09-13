6130a5893f1d920f50fc4900c49d03bf867440d0
djamkenny <beamahnenoch5@gmail.com>
2023-09-12 19:34:38 -0700

    put

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,6 +6,7 @@
 void print_times_table(int n)
 {
 	int i, j, k;
+
 	if (n >= 0 && n <= 15)
 {
 	for (i = 0; i <= n; i++)
@@ -20,7 +21,6 @@ void print_times_table(int n)
 	else if (k < 10 && j != 0)
 {
 	_putchar(',');
-	_putchar(' ');
 	_putchar(' ');
 	_putchar(' ');
 	_putchar(k + '0');
