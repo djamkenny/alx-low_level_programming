0b9e9a09a35d261609f45bb109d0a9f78ceac490
djamkenny <beamahnenoch5@gmail.com>
2023-09-14 12:42:42 -0700

    more fun

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isupper - check for uppercase
+ *
+ * Return 1
+ * else return 0
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (0);
+	}
+	else 
+	{
+		return (0);
+	}
+}
