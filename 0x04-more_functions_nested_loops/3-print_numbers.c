32a03373db28be89081ccf842e5bc72bf42bb7a8
djamkenny <beamahnenoch5@gmail.com>
2023-09-14 16:58:23 -0700

    hey

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,14 +3,16 @@
 /**
  * print_numbers - prints the numbers, from 0 to 9
  *
- * Return always 0
+ * Return 0 to 9
  */
 
 void print_numbers(void)
 {
-	for (int i = 0; i <= 9; i++)
+	int i;
+
+	for (i = 0; i <= 9; i++)
 	{
 		_putchar(i + '0');
 	}
-	_putchar("\n");
+	_putchar('\n');
 }
