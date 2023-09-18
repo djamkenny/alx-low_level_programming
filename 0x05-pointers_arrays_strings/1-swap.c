3fbd77affa15f818bfad4a4a85b3d407d0593493
djamkenny <beamahnenoch5@gmail.com>
2023-09-18 13:02:21 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the values of two integers
+ * @a: value of int
+ * @b: value of int
+ * Return 0
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	*c = *a;
+	*a = *b;
+	*b = *a;
+}
---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: stirng
+ * Return: 0
+ */
+
+int _strlen(char *s)
+{
+	*s;
+
+	while (*s != '\0')
+	{
+		*s++;
+	}
+	return (0);
+}
