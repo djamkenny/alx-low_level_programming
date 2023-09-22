4c1c74427fb617389463ec557c590257115e31d5
djamkenny <beamahnenoch5@gmail.com>
2023-09-22 08:11:31 -0700

    pointers

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *  _strcmp - compares two strings
+ *  @s1: paramter
+ *  @s2: parameter
+ *  Return _strcmp
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 == *s2)
+	{
+	if (*s1 == '\0')
+	return 0;
+	s1++;
+	s2++;
+    }
