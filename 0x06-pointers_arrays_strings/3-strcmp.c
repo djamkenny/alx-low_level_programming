7fcbda696243dc754a51bca68b198e780e91debb
djamkenny <beamahnenoch5@gmail.com>
2023-09-22 08:19:33 -0700

    pointer

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,16 +4,17 @@
  *  _strcmp - compares two strings
  *  @s1: paramter
  *  @s2: parameter
- *  Return _strcmp
+ *  Return: _strcmp
  */
 int _strcmp(char *s1, char *s2)
 {
 	while (*s1 == *s2)
 	{
 	if (*s1 == '\0')
-	return 0;
+	return (0);
 	s1++;
 	s2++;
 	}
-	return (int)((unsigned char)*s1 - (unsigned char)*s2);
+	return ((int)((unsigned char)*s1 - (unsigned char)*s2));
+	return (_strcmp);
 }
