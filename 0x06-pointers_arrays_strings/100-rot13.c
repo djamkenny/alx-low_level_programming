9d29650ac2f2b198676df1588bfcd77caccd8d75
djamkenny <beamahnenoch5@gmail.com>
2023-09-22 15:30:55 -0700

    pointer

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * rot13 - Capitalizes all words of a string.
+ * @*: The string to be capitalized.
+ *
+ * Return: 0
+ */
+char *rot13(char *str)
+{
+	if (str)
+	{
+	for (char *p = str; *p; ++p)
+	{
+	char c = *p;
+
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		char base = (c >= 'a' && c <= 'z') ? 'a' : 'A';
+		*p = ((c - base + 13) % 26) + base;
+	}
+	}
+	}
+	return str;
+}
