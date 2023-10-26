94ccefca951465f8584fabebfb6e6d6016a06bc3
djamkenny <beamahnenoch5@gmail.com>
2023-10-26 13:18:33 -0700

    :q:

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,12 +1,27 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-unsigned int binary_to_uint(const char *b);
+#include <stddef.h>
+/**
+ * struct listint_s - struct typedef
+ * @n: paramter
+ * @next: parameter
+ */
+
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+/* Function prototypes */
 int _putchar(char c);
+unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
-unsigned int flip_bits(unsigned long int n, unsigned long int m);
-#endif
+
+#endif /* MAIN_H */
+
